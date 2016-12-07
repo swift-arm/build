f2f6abe783c3a28a5b2e28a4c557c685014cf82a
William Dillon <william@housedillon.com>
2016-12-07 04:50:08 +0000

    Add logic to add Apple repos

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -6,6 +6,12 @@ REPOS="clang compiler-rt"
 for repo in $REPOS
 do
   pushd $repo
+    REMOTES=`git remote -v | wc -l`
+    if [ $REMOTES -lt "4" ]; then
+      echo Adding Apple repos
+      git remote add apple `git remote -v | head -n1 | sed 's/Swift-Arm/apple/' | sed 's/origin//' | sed 's/(fetch)//'`
+    fi
+
     echo Attempting to rebase $repo
     git fetch apple
     git rebase apple/stable
@@ -18,6 +24,12 @@ REPOS="cmark llbuild swift-corelibs-foundation swift-corelibs-xctest swiftpm lld
 for repo in $REPOS
 do
   pushd $repo
+    REMOTES=`git remote -v | wc -l`
+    if [ $REMOTES -lt "4" ]; then
+      echo Adding Apple repos
+      git remote add apple `git remote -v | head -n1 | sed 's/Swift-Arm/apple/' | sed 's/origin//' | sed 's/(fetch)//'`
+    fi
+
     echo Attempting to rebase $repo
     git fetch apple
     git rebase apple/master
