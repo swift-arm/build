fb9fbbbab8542cba7c8412bca84237f85045772d
Florian Friedrich <ffried@me.com>
2017-04-14 12:42:15 +0200

    refactor build script
    - Use custom buildbot for ARM builds
    - Abort when swift build-script failed
    - Make whitespaces in paths less of a problem
    - Move fixing package into function
    - Ensure correct directory by wrapping into pushd/popd

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,36 +1,54 @@
 #!/bin/bash
-pushd `dirname $0` > /dev/null
-WHERE_I_AM=`pwd`
-ARCH=`arch`
-popd > /dev/null
-INSTALL_DIR=${WHERE_I_AM}/../install
-PACKAGE=${WHERE_I_AM}/../swift.tar.gz
-LSB_RELEASE=`lsb_release -rs  | tr -d .`
-rm -rf $INSTALL_DIR $PACKAGE
-if [[ $ARCH =~ armv7 ]]; then
-echo "+ Building for ARM"
-# This is the buildbot we used to run, but its broken right now
-#./swift/utils/build-script --preset=buildbot_linux_armv7 install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
 
-./swift/utils/build-script --build-subdir buildbot_linux -R --lldb --llbuild --xctest --swiftpm --foundation --libdispatch -- --install-libdispatch --install-foundation --install-swift --install-lldb --install-llbuild --install-xctest --install-swiftpm --install-prefix=/usr '--swift-install-components=autolink-driver;compiler;clang-builtin-headers;stdlib;swift-remote-mirror;sdk-overlay;dev' --build-swift-static-stdlib --build-swift-static-sdk-overlay --skip-test-lldb --install-destdir=${INSTALL_DIR} --installable-package=${PACKAGE}
+pushd "$(dirname $0)/../" > /dev/null
 
-echo "+ Fixing up the install package for ARM"
-cp -R swift-corelibs-libdispatch/dispatch/ ${INSTALL_DIR}/usr/lib/swift
+BUILD_ROOT="$(pwd)/build"
+INSTALL_DIR="$(pwd)/install"
+PACKAGE="$(pwd)/swift.tar.gz"
 
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/swift/Dispatch.swiftdoc ${INSTALL_DIR}/usr/lib/swift/linux/armv7/
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/swift/Dispatch.swiftmodule ${INSTALL_DIR}/usr/lib/swift/linux/armv7/
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/libdispatch.la ${INSTALL_DIR}/usr/lib/swift/linux/
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/.libs/libdispatch.so ${INSTALL_DIR}/usr/lib/swift/linux
+export SWIFT_BUILD_ROOT="${BUILD_ROOT}"
 
-mkdir -p ${INSTALL_DIR}/usr/lib/swift/os
-cp swift-corelibs-libdispatch/os/linux_base.h ${INSTALL_DIR}/usr/lib/swift/os
-cp swift-corelibs-libdispatch/os/object.h ${INSTALL_DIR}/usr/lib/swift/os
+fix_arm_install_package() {
+	mkdir -p "${INSTALL_DIR}/usr/lib/swift"
+	cp -R "./swift-corelibs-libdispatch/dispatch/" "${INSTALL_DIR}/usr/lib/swift/"
 
-# Retar
-echo "+ Retar installation package"
-tar -C ${INSTALL_DIR} -czf ${PACKAGE} .
+	DISPATCH_SRC="${BUILD_ROOT}/buildbot_linux/libdispatch-linux-armv7/src"
+	mkdir -p "${INSTALL_DIR}/usr/lib/swift/linux/armv7"
+	cp "${DISPATCH_SRC}/swift/Dispatch.swiftdoc" "${DISPATCH_SRC}/swift/Dispatch.swiftmodule" "${INSTALL_DIR}/usr/lib/swift/linux/armv7/"
+	cp "${DISPATCH_SRC}/libdispatch.la" "${DISPATCH_SRC}/.libs/libdispatch.so" "${INSTALL_DIR}/usr/lib/swift/linux/"
 
+	mkdir -p "${INSTALL_DIR}/usr/lib/swift/os"
+	cp "./swift-corelibs-libdispatch/os/linux_base.h" "./swift-corelibs-libdispatch/os/object.h" "${INSTALL_DIR}/usr/lib/swift/os"
+}
+
+echo "=> Cleaning previous installation folder and installation package"
+rm -rf "${INSTALL_DIR}" "${PACKAGE}"
+
+if [[ $(arch) =~ armv7 ]]; then
+	echo "=> Building for ARM"
+
+	PRESETS_FILE="./etc/arm_buildbots.ini"
+	if [ -e "${PRESETS_FILE}" ]; then
+	    PRESET_OPTION="--preset-file=${PRESETS_FILE}"
+	fi
+	./swift/utils/build-script ${PRESET_OPTION} --preset=buildbot_linux_armv7 install_destdir="${INSTALL_DIR}" installable_package="${PACKAGE}"
+
+	RETVAL=$?
+	if [ $RETVAL -ne "0" ]; then
+		echo "=> Build failed!"
+		popd > /dev/null
+		exit $RETVAL
+	fi
+
+	echo "=> Fixing up the install package for ARM"
+	fix_arm_install_package
+
+	echo "=> Retar installation package"
+	tar -czf "${PACKAGE}" "${INSTALL_DIR}" --owner=0 --group=0
 else
-echo "+ Building for x86"
-./swift/utils/build-script --preset=buildbot_linux_${LSB_RELEASE} install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
+	LSB_RELEASE=`lsb_release -rs  | tr -d .`
+	echo "=> Building for x86 with linux_${LSB_RELEASE} buildbot"
+	./swift/utils/build-script --preset=buildbot_linux_${LSB_RELEASE} install_destdir="${INSTALL_DIR}" installable_package="${PACKAGE}"
 fi
+
+popd > /dev/null
